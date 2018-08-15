a105f948df3154fc79b85f73dfbdb0efb4a0e13d
craca651 <craca651@student.otago.ac.nz>
2018-08-15 16:17:21 +1200

    Set up contact_data page so user information is import from to ContactsPage

--- lib/ContactPage.dart
+++ lib/ContactPage.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:outreachcrm_app/contact_data.dart';
 
-class ContactsPage extends StatefulWidget {
-  @override
-  _SearchBar createState() => new _SearchBar();
-}
-
-
-class _SearchBar extends State<ContactsPage> {
-  Widget appBarTitle = new Text("Search Contacts...");
-  Icon actionIcon = new Icon(Icons.search);
+class ContactsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-          centerTitle: true,
-          title:appBarTitle,
-          actions: <Widget>[
-            new IconButton(icon: actionIcon,onPressed:(){
-              setState(() {
-                if ( this.actionIcon.icon == Icons.search){
-                  this.actionIcon = new Icon(Icons.close);
-                  this.appBarTitle = new TextField(
-                    style: new TextStyle(
-                      color: Colors.white,
+        appBar: new AppBar(
+          title: new Text("Contacts"),
+        ),
+        body: new ContactList(kContacts));
+  }
+}
 
-                    ),
-                    decoration: new InputDecoration(
-                        prefixIcon: new Icon(Icons.search,color: Colors.white),
-                        hintText: "Search...",
-                        hintStyle: new TextStyle(color: Colors.white)
-                    ),
-                  );}
-// Code for search bar from: https://stackoverflow.com/questions/49966980/how-to-create-toolbar-searchview-in-flutter*/
+class ContactList extends StatelessWidget {
+  final List<Contact> _contacts;
 
-              });
-            } ,),]
-      ),
+  ContactList(this._contacts);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      padding: new EdgeInsets.symmetric(vertical: 8.0),
+      itemBuilder: (context, index) {
+        return new _ContactListItem(_contacts[index]);
+      },
+      itemCount: _contacts.length,
     );
   }
 }
 
+class _ContactListItem extends ListTile {
+  _ContactListItem(Contact contact)
+      : super(
+      title: new Text(contact.fullName),
+      subtitle: new Text(contact.email),
+      leading: new CircleAvatar(child: new Text(contact.fullName[0])));
+}
