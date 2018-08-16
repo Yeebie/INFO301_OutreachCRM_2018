a1c7fcdecbd6121e0d2f51a3e88c9cb55002d73a
poujo381 <poujo381@student.otago.ac.nz>
2018-08-16 21:56:49 +1200

    Added button functionality to fields (doesn't do anything yet). Small visual changes. Added a dependency for calling within app (might need to change it to a different one later) #10

--- lib/viewContact.dart
+++ lib/viewContact.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,9 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final title = 'Name Namerson';
     final mobilePh = '027 452 4318';
-    final homePh = '07 645 8524';
+    final homePh = "076458524";
     final workPh = '07 645 8524';
     final emailAd = 'namerson@gmail.com';
+    final client = 'Thomas Green Industries';
 
 
 
@@ -26,38 +28,95 @@ class MyApp extends StatelessWidget {
 
             ListTile(
               leading: Icon(Icons.home),
-              title: Text('Thomas Green Industries'),
+              title: Text(client,  style: new TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
 
             ListTile(
-              leading: Icon(Icons.email),
-              title: Text(emailAd),
+              leading: new IconButton(
+                icon: new Icon(Icons.note),
+                tooltip: 'View client notes',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ClientNotes()),
+                  );
+                },
+              ),
+              title: Text('Notes',  style: new TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
+
 
             new Text(
               "General",
-              style: new TextStyle(fontSize:32.0,
+              style: new TextStyle(fontSize:25.0,
                   color: const Color(0xFF000000),
                   fontWeight: FontWeight.w500,
-                  fontFamily: "Roboto"),
+                  fontFamily: "Roboto",
+                  height: 2.0),
             ),
 
             ListTile(
-              leading: Icon(Icons.mobile_screen_share),
-              title: Text(mobilePh),
+              //leading: Icon(Icons.mobile_screen_share),
+              leading: new IconButton(
+                icon: new Icon(Icons.mobile_screen_share),
+                tooltip: 'Increase volume by 10%',
+                //onPressed: () { setState(() { _volume *= 1.1; }); },
+              ),
+              title: Text(mobilePh + ' (Mobile)', style: new TextStyle(color: Colors.green),
+              ),
             ),
 
             ListTile(
-              leading: Icon(Icons.phone),
-              title: Text(workPh + '(Work)'),
+              leading: new IconButton(
+                icon: new Icon(Icons.phone),
+                tooltip: 'Increase volume by 10%',
+                //onPressed: () { setState(() { _volume *= 1.1; }); },
+              ),
+              title: Text(workPh + ' (Work)', style: new TextStyle(color: Colors.green),
+              ),
             ),
 
             ListTile(
-              leading: Icon(Icons.phone),
-              title: Text(homePh + ' (Home)'),
+              leading: new IconButton(
+                icon: new Icon(Icons.phone),
+                tooltip: 'Increase volume by 10%',
+                onPressed: () => launch(homePh),
+              ),
+              title: Text(homePh + ' (Home)',style: new TextStyle(color: Colors.green),
+              ),
+            ),
+
+            ListTile(
+              leading: new IconButton(
+                icon: new Icon(Icons.email),
+                tooltip: 'Increase volume by 10%',
+                //onPressed: () { setState(() { _volume *= 1.1; }); },
+              ),
+              title: Text(emailAd, style: new TextStyle(color: Colors.blue),),
             ),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ClientNotes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Thomas Green Industries"), //Obvs won't text input the name
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
         ),
       ),
     );
