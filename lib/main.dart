da45d8138d619aa1fac53d2e65bf1dfba863f409
Yeebo <yeeja359@student.otago.ac.nz>
2018-08-31 04:23:24 +1200

    Solved API Key Retrieval issue, added retrieved values from API Validation into validation methods.
    outreachcrm_app:
      outreach > lib > ...
      - main.dart                      | Moved API Validation around, utilised retrieved values.
      outreach > ...
      - pubspec.yaml                   | Updated http from .16 to .17 (New Dart Patch).
    
    References: #15
    Resolves: #15
    
    Notes: (´・ω・)っ由 | Trading clean hats for 1.33 ref, !bp me.
    - The problem was that we were doing API validation on a null value.
    - When we successfully logged in, we were logging in with the first API Key, and running Validation on the second.
    - I've shifted the code around so the APIKeyRetrieval widget calls the APIKeyValidation widget once its done.
    - I'm having some trouble with the ContactsListRetrieval widget, some of the code isn't running.
    - The Contacts Page just opens before the ContactList has been decoded, this'll be fixed later.
    - We should probably read in the json error messages, and use them for our errors. Just a thought.
    - Aaron, I'm so sorry I'm going to be super tired when we meet up, I just really wanted to fix this issue.

--- lib/main.dart
+++ lib/main.dart
@@ -46,12 +46,25 @@ class APIKeyFields {
   bool _passwordVerify = false;
 }
 
+class APIKeyValidationFields {
+  bool _verify = false;
+  String _expiry = '';
+  String _oid = '';
+}
+
+///Make this an array? How can we do this?
+class ContactListFields {
+  String _name_processed = '';
+}
+
 class LoginPageState extends State<LoginPage>
     with SingleTickerProviderStateMixin {
   //Datafields
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   LoginFields _fields = new LoginFields();
   APIKeyFields _apiKeyFields = new APIKeyFields();
+  APIKeyValidationFields _apiKeyValidationFields = new APIKeyValidationFields();
+  ContactListFields _contactListFields = new ContactListFields();
 
   Map<String, dynamic> apiKey;
 
@@ -142,6 +155,7 @@ class LoginPageState extends State<LoginPage>
       if (this._formKey.currentState.validate()) {
         //Saves the form
         this._formKey.currentState.save();
+        print("");
         print('Login Details');
         print('Username: ${_fields._username}');
         print('Password: ${_fields._password}');
@@ -150,39 +164,6 @@ class LoginPageState extends State<LoginPage>
 
         ///Retrieve the API Key
         _getAPIKeyRetrieval();
-
-        //Defining regex to search for key
-        RegExp apiPattern = new RegExp(
-          r"([a-z0-9]){32}",
-          caseSensitive: false,
-          multiLine: false,
-        );
-
-        ///Start troublesome code (Can we delay this validation until we actually get the API Key?)
-        //If the pattern matches the key we got a valid request!
-        if (apiPattern.hasMatch(_apiKeyFields._apiKey)) {
-          print("I'm logging in");
-
-          ///Verify API Key
-          _getAPIKeyVerification();
-
-          ///Get Contacts List
-          _getContactsList();
-
-          //Successful login
-          usernameController.clear();
-          passwordController.clear();
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ContactsPage()),
-          );
-        }
-        //Otherwise unsuccessful login
-        else {
-          showDialogParent(
-              "Incorrect login", "Couldn't verify username or password");
-        }
-        ///End troublesome code
       }
     } catch (e) {
       showDialogParent("Error", "Something bad happened");
@@ -264,77 +245,122 @@ class LoginPageState extends State<LoginPage>
     );
   }
 
+  ///***************************************************************************
+  ///                  A P I   K E Y   R E T R I E V A L
+  ///***************************************************************************
+
+
   ///Retrieving API Key
   Widget _getAPIKeyRetrieval() {
     //Kind of like a method, will do all sorts of fantastic things in the future
-    Future<String> _loadAPIKeyAsset() async {
-      //Creating the URL that'll query the database for our API Key
-      String _requestAPIKeyRetrieval = "https://" +
-          _fields._domain +
-          ".outreach.co.nz/api/0.2/auth/login/?username=" +
-          _fields._username +
-          "&password=" +
-          _fields._password;
-      print('Creating the URL to generate API Keys via Login Details: ' +
-          _requestAPIKeyRetrieval);
-      print('\n\n');
+    //Creating the URL that'll query the database for our API Key
+    String _requestAPIKeyRetrieval = "https://" +
+        _fields._domain +
+        ".outreach.co.nz/api/0.2/auth/login/?username=" +
+        _fields._username +
+        "&password=" +
+        _fields._password;
+    print("API Key Retrieval");
+    print('Creating the URL to generate API Keys via Login Details: ' +
+        _requestAPIKeyRetrieval);
 
-      http.post(_requestAPIKeyRetrieval).then((response) {
-        print("API Key Retrieval");
-        //Print the API Key, just so we can compare it to the final result
-        print("Original Response body: ${response.body}");
-        //Turning the json into a map
-        Map jsonResponse = json.decode(response.body);
-        //Getting the data from ['data'], which happens to be our array
-        Data data = new Data.fromJson(jsonResponse['data']);
-        //Applying the data from the json to the instance of the Data class
-        _apiKeyFields._apiKey = data.getAPIKey();
-        _apiKeyFields._expiry = data.getExpiry();
+    ///This section is delayed until first login attempt has been done, results in invalid verification
+    http.post(_requestAPIKeyRetrieval).then((response) {
+      //Print the API Key, just so we can compare it to the final result
+      print("Original Response body: ${response.body}");
+      //Turning the json into a map
+      Map apiKeyRetrievalMap = json.decode(response.body);
+      //Getting the data from ['data'], which happens to be our array
+      APIKeyRetrievalData data =
+          new APIKeyRetrievalData.fromJson(apiKeyRetrievalMap['data']);
+      //Applying the data from the json to the instance of the Data class
+      _apiKeyFields._apiKey = data.getAPIKey();
+      _apiKeyFields._expiry = data.getExpiry();
 
-        //Removing null value, just in case we need to use this datafield
-        if(data.getPasswordVerify() == false) {
-          _apiKeyFields._passwordVerify = false;
-        } else {
-          _apiKeyFields._passwordVerify = true;
-        }
+      //Removing null value, just in case we need to use this datafield
+      if (data.getPasswordVerify() == false) {
+        _apiKeyFields._passwordVerify = false;
+      } else {
+        _apiKeyFields._passwordVerify = true;
+      }
 
-        //Retrieving the API Key from the array
-        print("Printing getAPIKey()");
-        print(_apiKeyFields._apiKey);
-        print("Printing getExpiry()");
-        print(_apiKeyFields._expiry);
-        print("Printing getPasswordVerify()");
-        print(_apiKeyFields._passwordVerify);
-      });
-    }
+      //Retrieving the API Key from the array
+      ///This parts important, we're generating 2 API Keys, this is the one from this login attempt
+      print("Printing getAPIKey()");
+      print(_apiKeyFields._apiKey);
+      print("Printing getExpiry()");
+      print(_apiKeyFields._expiry);
+      print("Printing getPasswordVerify()");
+      print(_apiKeyFields._passwordVerify);
+      print('\n \n');
 
-    //Calling the method we just wrote
-    _loadAPIKeyAsset();
+      if(_apiKeyFields._passwordVerify == false) {
+        showDialogParent(
+            "Baka!", "Couldn't verify username or password.");
+      } else {
+        ///Verify API Key
+        _getAPIKeyVerification();
+      }
+    });
   }
 
+  ///***************************************************************************
+  ///                  A P I   K E Y   V A L I D A T I O N
+  ///***************************************************************************
+
   ///Validating API Key
-  ///Currently responds with {"data":{"verify":false},"error":"Contact deactivated"}, our account may be archived or deleted
   Widget _getAPIKeyVerification() {
     //Next step is to verify the key, retrieve the user etc
-    print('\n \n');
-    print('API Key Validation\n');
+    print('API Key Validation');
     String _requestAPIKeyVerification = "https://" +
         _fields._domain +
         ".outreach.co.nz/api/0.2/auth/verify/?apikey=" +
         _apiKeyFields._apiKey;
     print('API Validation URL: ' + _requestAPIKeyVerification);
-    print('\n \n');
 
     http.post(_requestAPIKeyVerification).then((response) {
       //Print the API Key, just so we can compare it to the subset String
       print("API Key Verification Response: ${response.body}");
       //Turning the json into a map
       Map apiKeyVerificationMap = json.decode(response.body);
-      print(apiKeyVerificationMap);
-      print('\n \n');
-      return (apiKeyVerificationMap);
+      //Getting the data from ['data'], which happens to be our array
+      APIKeyValidationData data =
+      new APIKeyValidationData.fromJson(apiKeyVerificationMap['data']);
+      //Applying the data from the json to the instance of the Data class
+      _apiKeyValidationFields._verify = data.getVerify();
+      _apiKeyValidationFields._expiry = data.getExpiry();
+      _apiKeyValidationFields._oid = data.getOid();
+
+      //Defining regex to search for key
+      RegExp apiPattern = new RegExp(
+        r"([a-z0-9]){32}",
+        caseSensitive: false,
+        multiLine: false,
+      );
+
+      print("Current API Key");
+      print(_apiKeyFields._apiKey);
+      print("Printing Verify");
+      print(_apiKeyValidationFields._verify);
+      print("Printing Expiry");
+      print(_apiKeyValidationFields._expiry);
+      print("Printing OID");
+      print(_apiKeyValidationFields._oid);
+
+      if (apiPattern.hasMatch(_apiKeyFields._apiKey)) {
+        print("API Key Matches Format");
+        print("\n\n");
+        _getContactsList();
+      } else {
+        showDialogParent(
+            "Is this statement even possible?", "API Key isn't valid.");
+      }
     });
   }
+
+  ///***************************************************************************
+  ///             C O N T A C T S   L I S T   R E T R I E V A L
+  ///***************************************************************************
 
   ///Loading the Contacts List into a Collection
   Widget _getContactsList() {
@@ -346,41 +372,52 @@ class LoginPageState extends State<LoginPage>
         _apiKeyFields._apiKey +
         "&properties=%5B%27name_processed%27%5D&conditions=%5B%5B%27status%27,%27=%27,%27O%27%5D,%5B%27oid%27,%27%3E=%27,%27100%27%5D%5D";
 //    Display [user's names], if their [status is open] and their [oid is larger than 100]
-//    Encode doesn't convert apostrophes, it may be easier to write the query by hand, then CTRL + H all the necessary bits
-//    %5B = [
-//    %5D = ]
-//    %27 = '
+//    Dart Encode doesn't convert apostrophes, it may be easier to write the query by hand, then CTRL + H all the necessary bits
+//    Can we do this conversion cleanly in app?
+//    %5B = "["
+//    %5D = "]"
+//    %27 = "'"
+//    %20 = " "
 
     print('Get Contact List URL: ' + _requestContactList);
-    print('\n \n');
+
+    ///This section is temporary, for some reason, some of the code below the post doesn't execute
+    ///Display Contacts Page
+    usernameController.clear();
+    passwordController.clear();
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ContactsPage()),
+    );
 
     http.post(_requestContactList).then((response) {
       //Print the API Key, just so we can compare it to the subset String
-      print('\n \n');
       print("Contact List Response:");
       print(response.body);
 
       //Turning the json into a map
       Map<String, dynamic> contactListMap = json.decode(response.body);
-      print("Contact List Map: ");
-      print(contactListMap);
-      print('\n \n');
+      //Getting the data from ['data'], which happens to be our array
+      ContactListData data =
+      new ContactListData.fromJson(contactListMap['data']);
+      //Applying the data from the json to the instance of the Data class
+      print("Processing getNameProcessed()");
+      print(data.getNameProcessed());
+      _contactListFields._name_processed = data.getNameProcessed();
 
-      print("Printing all contacts in Map");
-      print(contactListMap['data']);
-      print('\n \n');
     });
   }
 }
 
+///API Key Retrieval
 ///Represents the bits inside the nested json
-class Data {
+class APIKeyRetrievalData {
   String key;
   String expiry;
   bool passwordVerify;
 
   //Constructor
-  Data({this.key, this.expiry, this.passwordVerify});
+  APIKeyRetrievalData({this.key, this.expiry, this.passwordVerify});
 
   //Getter method
   String getAPIKey() {
@@ -398,8 +435,8 @@ class Data {
   }
 
   //Soft of like a method that'll be executed somewhere
-  factory Data.fromJson(Map<String, dynamic> json) {
-    return Data(
+  factory APIKeyRetrievalData.fromJson(Map<String, dynamic> json) {
+    return APIKeyRetrievalData(
         key: json['key'],
         expiry: json['expiry'],
         passwordVerify: json['password']);
@@ -407,15 +444,100 @@ class Data {
 }
 
 ///Represents the base json, the data array
-class APIKeyJson {
+class APIKeyRetrievalJson {
   //Datafields
-  Data data;
+  APIKeyRetrievalData data;
 
   //Constructor
-  APIKeyJson({this.data});
+  APIKeyRetrievalJson({this.data});
 
   //Soft of like a method that'll be executed somewhere
-  factory APIKeyJson.fromJson(Map<String, dynamic> parsedJson) {
-    return APIKeyJson(data: Data.fromJson(parsedJson['data']));
+  factory APIKeyRetrievalJson.fromJson(Map<String, dynamic> parsedJson) {
+    return APIKeyRetrievalJson(
+        data: APIKeyRetrievalData.fromJson(parsedJson['data']));
+  }
+}
+
+///API Key Validation
+///Represents the bits inside the nested json
+class APIKeyValidationData {
+  bool verify;
+  String expiry;
+  String oid;
+
+  //Constructor
+  APIKeyValidationData({this.verify, this.expiry, this.oid});
+
+  //Getter method
+  bool getVerify() {
+    return verify;
+  }
+
+  //Getter method
+  String getExpiry() {
+    return expiry;
+  }
+
+  //Getter method
+  String getOid() {
+    return oid;
+  }
+
+  //Soft of like a method that'll be executed somewhere
+  factory APIKeyValidationData.fromJson(Map<String, dynamic> json) {
+    return APIKeyValidationData(
+        verify: json['verify'], expiry: json['expiry'], oid: json['oid']);
+  }
+}
+
+///Data is different between requests, we need to copy this format multiple times
+///Represents the base json, the data array
+class APIKeyValidationJson {
+  //Datafields
+  APIKeyValidationData data;
+
+  //Constructor
+  APIKeyValidationJson({this.data});
+
+  //Soft of like a method that'll be executed somewhere
+  factory APIKeyValidationJson.fromJson(Map<String, dynamic> parsedJson) {
+    return APIKeyValidationJson(
+        data: APIKeyValidationData.fromJson(parsedJson['data']));
+  }
+}
+
+///Contact List Retrieval
+///Represents the bits inside the nested json
+class ContactListData {
+  String name_processed;
+
+  //Constructor
+  ContactListData({this.name_processed});
+
+  //Getter method
+  String getNameProcessed() {
+    return name_processed;
+  }
+
+  //Soft of like a method that'll be executed somewhere
+  factory ContactListData.fromJson(Map<String, dynamic> json) {
+    return ContactListData(
+        name_processed: json['name_processed']);
+  }
+}
+
+///Data is different between requests, we need to copy this format multiple times
+///Represents the base json, the data array
+class ContactListJson {
+  //Datafields
+  ContactListData data;
+
+  //Constructor
+  ContactListJson({this.data});
+
+  //Soft of like a method that'll be executed somewhere
+  factory ContactListJson.fromJson(Map<String, dynamic> parsedJson) {
+    return ContactListJson(
+        data: ContactListData.fromJson(parsedJson['data']));
   }
 }
