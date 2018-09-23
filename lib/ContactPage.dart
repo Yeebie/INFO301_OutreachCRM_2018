f76f4ef4be8d51ac7b68790409d9769d99f96938
aaranderson <aaron@applabs.co.nz>
2018-09-23 20:22:18 +1200

    Implemented search functionality. Closes #37

--- lib/ContactPage.dart
+++ lib/ContactPage.dart
@@ -56,6 +56,18 @@ class _ContactPage extends State<_ContactsPage> {
   ScrollController controller;
   int count;
 
+  //Datafields for the app bar
+  final formKey = new GlobalKey<FormState>();
+  final key = new GlobalKey<ScaffoldState>();
+  final TextEditingController _filter = new TextEditingController();
+  final color = const Color(0xFF0085CA);
+  final white = const Color(0xFFFFFFFF);
+  Icon _searchIcon = new Icon(
+    Icons.search,
+    color: Colors.white,
+  );
+  Widget _appBarTitle = new Text('Contacts');
+
   //Constructor
   _ContactPage(String apiKey, String domain, List<Contact> kContacts) {
     this._apiKey = apiKey;
@@ -65,12 +77,20 @@ class _ContactPage extends State<_ContactsPage> {
 
   ///The list that holds all the contacts onscreen
   Widget _buildContacts() {
+    bool searching;
+    //Checks to see if we are in searching mode or not
+    if (this._searchIcon.icon == Icons.close) {
+      searching = true;
+    } else {
+      searching = false;
+    }
     print("buildContacts() empty list check");
     print("Checking if _contacts has any data");
     print("_contacts length: " + (_contacts.length).toString());
 
     ///The list starts with no data in it, since the getContactsList() method is in this class, we have to load it here
-    if (_contacts.length == 0) {
+    ///Added check to see if we are searching
+    if (_contacts.length == 0 && !searching) {
       print("_contacts is empty, requesting resupply");
       print("\n\n");
       getContactsList(0, _apiKey, _domain);
@@ -86,8 +106,11 @@ class _ContactPage extends State<_ContactsPage> {
         itemBuilder: (context, index) {
           //If we hit the end of the client's contacts list, the app'll spam getContactsList()
           //This is due to the List staying at the max index and triggering the call over and over
-          //count tracks the size of the last API call, you get the point
-          if (index >= (_contacts.length - 1) && count != 0) {
+          //count tracks the size of the last API call, you get the point.
+
+          //Added check to see if we are searching or not as we don't want to paginate if
+          //we are searching
+          if (index >= (_contacts.length - 1) && count != 0 && !searching) {
             print("Pagination get!");
             print("\n");
             getContactsList(index, _apiKey, _domain);
@@ -107,6 +130,44 @@ class _ContactPage extends State<_ContactsPage> {
     }
   }
 
+  //This method gets called when the search icon is pressed
+  //It reveals a text field and allows a user to search their contacts
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close, color: Colors.white);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          style: new TextStyle(
+            color: Colors.white,
+          ),
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search, color: Colors.white),
+              hintText: 'Search...',
+              hintStyle: new TextStyle(color: Colors.white),
+              labelStyle: new TextStyle(color: Colors.white)),
+          onChanged: (value) {
+            if (value.length > 0) {
+              searchContactsList(_apiKey, _domain, value);
+              _buildContacts();
+            } else {
+              _contacts.clear();
+              getContactsList(0, _apiKey, _domain);
+              _buildContacts();
+            }
+          },
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Contacts');
+        _filter.clear();
+        _contacts.clear();
+        getContactsList(0, _apiKey, _domain);
+        _buildContacts();
+      }
+    });
+  }
+
   ///Stylised contact item
   Widget buildRow(Contact contact) {
     return ListTile(
@@ -117,48 +178,28 @@ class _ContactPage extends State<_ContactsPage> {
     );
   }
 
+  //Method for building a custom app bar
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      backgroundColor: color,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+        color: Colors.white,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final color = const Color(0xFF0085CA);
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new Scaffold(
-          appBar: new AppBar(
-            title: widget.appBarTitle,
-            backgroundColor: color,
-            actions: <Widget>[
-              new IconButton(
-                icon: widget.actionIcon,
-                onPressed: () {
-                  setState(() {
-                    if (widget.actionIcon.icon == Icons.search) {
-                      widget.actionIcon = new Icon(Icons.close);
-                      widget.appBarTitle = new TextField(
-                        style: new TextStyle(
-                          color: Colors.white,
-                        ),
-                        decoration: new InputDecoration(
-                            prefixIcon:
-                                new Icon(Icons.search, color: Colors.white),
-                            hintText: "Search...",
-                            hintStyle: new TextStyle(color: Colors.white)),
-                        onChanged: (value) {
-                          print(value);
-                          //filter your contact list based on value
-                        },
-                      );
-                    } else {
-                      widget.actionIcon =
-                          new Icon(Icons.search); //reset to initial state
-                      widget.appBarTitle = new Text("Contacts");
-                    }
-                  });
-                },
-              ),
-            ],
-          ),
-
-          ///Used to be called kContacts
+          resizeToAvoidBottomPadding: false,
+          appBar: _buildBar(context),
           body: _buildContacts()),
     );
   }
@@ -237,6 +278,64 @@ class _ContactPage extends State<_ContactsPage> {
       print("\n");
 
       ///Add the new contacts to the current List, refresh list
+      _contacts.addAll(contactsList);
+      setState(() {});
+    });
+  }
+
+  ///This function will search the users contacts list based
+  ///on the input they put in the search box.
+  Future<Contact> searchContactsList(
+      String _apiKey, String _domain, String query) async {
+    String apikey = "?apikey=" + _apiKey;
+
+    String properties = "&properties=" +
+        "['oid','name_processed','modified']" +
+        "&search=['OR',['o_first_name','like','" +
+        query +
+        "'],['o_last_name','like','" +
+        query +
+        "'],['name_processed','like','" +
+        query +
+        "']]&order=[['modified','DESC'],['o_first_name','=','DESC'],['o_last_name','=','DESC']]&limit=20";
+
+    print('Retrieving Contacts List');
+
+    ///Specifying the URL we'll make to call the API
+    String _requestContactList = "https://" +
+        _domain +
+        ".outreach.co.nz/api/0.2/query/user" +
+        (apikey + properties);
+
+    ///Encoding the String so its HTML safe
+    _requestContactList = _requestContactList.replaceAll("[", "%5B");
+    _requestContactList = _requestContactList.replaceAll("]", "%5D");
+    _requestContactList = _requestContactList.replaceAll("'", "%27");
+    _requestContactList = _requestContactList.replaceAll(">", "%3E");
+    print('Get Contact List URL: ' + _requestContactList);
+
+    ///Send an API request, load all of the json into a map
+    http.post(_requestContactList).then((response) {
+      //Print the API Key, just so we can compare it to the subset String
+      print("Contact List Response:");
+      print(response.body);
+      List<Contact> contactsList = new List();
+      //Turning the json into a map
+      final contactListMap = json.decode(response.body);
+      ContactListJson contactListJson =
+          new ContactListJson.fromJson(contactListMap);
+      print("\n");
+
+      ///Creates a new contact filled with data, adds it to List<Contact>
+      for (ContactListData data in contactListJson.data) {
+        Contact contact = new Contact();
+        contact.setFullName(data.getNameProcessed());
+        contact.setOid(data.getOid());
+        contact.setCompany(data.getCompany());
+        contactsList.add(contact);
+      }
+
+      _contacts.clear();
       _contacts.addAll(contactsList);
       setState(() {});
     });
