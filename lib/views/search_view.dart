999ca4f166dc523ee3a0e6a9021835b9edce1b04
sammykerridge <sammykerridge@gmail.com>
2018-11-19 20:43:38 +1300

    added a method for searching

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:outreach/models/contact.dart';
 import 'package:outreach/view-models/search_state.dart';
 import 'package:outreach/views/widgets/appbar.dart';
 
@@ -7,6 +8,7 @@ class SearchView extends SearchState {
   @override
   Widget build(BuildContext context) {
     Size phoneSize = MediaQuery.of(context).size;
+    List<Contact> contacts = doContactSearch() ?? new List();
     final double statusbarHeight = MediaQuery
           .of(context).padding.top;
 
